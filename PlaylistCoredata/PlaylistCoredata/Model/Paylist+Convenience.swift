5d8b1349dca791241a0816ccd59857a1453ef3f5
gklyons <30358106+gklyons@users.noreply.github.com>
2020-03-04 12:03:07 -0700

    Song Playlist Convenience

---
+++ PlaylistCoredata/PlaylistCoredata/Model/Paylist+Convenience.swift
@@ -0,0 +1,22 @@
+//
+//  Paylist+Convenience.swift
+//  PlaylistCoredata
+//
+//  Created by Garrett Lyons on 3/4/20.
+//  Copyright © 2020 Trevor Walker. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Playlist {
+    
+    convenience init(name: String, songs: [Song] = [], moc: NSManagedObjectContext = CoreDataStack.context) {
+        self.init(context:moc)
+        self.name = name
+        
+    }
+}
+
+
+
