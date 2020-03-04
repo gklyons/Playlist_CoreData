d4a12e5df64c39aee310c628a1c61e2b4768b52d
gklyons <30358106+gklyons@users.noreply.github.com>
2020-03-04 14:10:15 -0700

    Storyboard

---
+++ PlaylistCoredata/PlaylistCoredata/Model/PlaylistViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PlaylistViewController.swift
+//  PlaylistCoredata
+//
+//  Created by Garrett Lyons on 3/4/20.
+//  Copyright © 2020 Trevor Walker. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistViewController: UIViewController {
+    @IBOutlet weak var playlistTableView: UITableView!
+    @IBOutlet weak var playlistNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
